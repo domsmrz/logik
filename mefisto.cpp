9f376579ce0fd1a84a8b05281aadbfe010099eb1
Dominik Smrž <dominik.smrz@leris.cz>
2015-03-17 19:37:21 +0100

    Úprava na funkční mefisto

--- mefisto.cpp
+++ mefisto.cpp
@@ -1,55 +1,127 @@
+#include <cstdio>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
+#include <cmath>
+#include <algorithm>
+#include <list>
+
 #include "mefisto.h"
 #include "macros.h"
 
-Mefisto::Mefisto(int my_ai_id, int my_colors, int my_pegs) {
-	ai_id = my_ai_id;
-	colors = my_colors;
-	pegs = my_pegs;
-	correct = 0;
+Mefisto::Mefisto(int ai_id, int colors, int pegs) {
+	this->ai_id = ai_id;
+	this->colors = colors;
+	this->pegs = pegs;
+	this->correct = 0;
+	this->nseq = (int) pow(this->colors, this->pegs);
 
-	for (int i = 0; i < colors; ++i) {
-		colors_ans[i] = 0;
-	}
+	this->prepare_allowed_seq();
 
-	srand (time(NULL));
-	for (int i = 0; i < pegs; ++i) {
-		answer[i] = rand() % colors;
-		colors_ans[answer[i]]++;
-	}
+	this->answers = new int[(this->pegs+1) * (this->pegs+1)]();
 }
 
+/*** PRIVATE ***/
+
+void Mefisto::prepare_allowed_seq(void) {
+	int i;
+
+	for (i = 0; i < this->nseq; ++i) {
+		this->allowed_seq.push_back(i);
+	}
+
+	return;
+}
+
+int Mefisto::get_answer(int final, int* query) {
+	int i;
+
+	int final_seq[this->pegs];
+	for (i = this->pegs - 1; i >= 0; --i) {
+		final_seq[i] = final % this->colors;
+		final /= this->colors;
+	}
+
+	int correct_pegs = 0;
+	int correct_colors = 0;
+
+	for (i = 0; i < this->pegs; ++i) {
+		if (final_seq[i] == query[i])
+			correct_pegs++;
+	}
+
+	int final_colors[this->colors];
+	int query_colors[this->colors];
+	for (i = 0; i < this->colors; ++i) {
+		final_colors[i] = query_colors[i] = 0;
+	}
+
+	for (i = 0; i < this->pegs; ++i) {
+		final_colors[final_seq[i]]++;
+		query_colors[query[i]]++;
+	}
+
+	for (i = 0; i < this->colors; ++i) {
+		correct_colors += std::min(final_colors[i], query_colors[i]);
+	}
+	correct_colors -= correct_pegs;
+
+	return correct_pegs * (this->pegs+1) + correct_colors;
+}
+
+
+/*** PUBLIC ***/
+
 int* Mefisto::get_reply(int* query) {
-	int* reply = (int*) calloc(pegs, sizeof(int));
-	int copy_colors[colors];
-	for (int i = 0; i < colors; ++i) {
-		copy_colors[i] = colors_ans[i];
+	int i;
+	int answerI;
+	std::list<int>::iterator allowed_seq_it;
+
+	for (allowed_seq_it = this->allowed_seq.begin(); allowed_seq_it != this->allowed_seq.end(); ++allowed_seq_it) {
+		answerI = this->get_answer(*allowed_seq_it, query);
+		this->answers[answerI]++;
 	}
 
-	int reply_i = 0;
+	int max = -1;
+	int maxI = -1;
 
-	for (int i = 0; i < pegs; ++i) {
-		if (query[i] == answer[i]) {
-			copy_colors[query[i]]--;
-			reply[reply_i++] = 2;
+	for (i = 0; i < (this->pegs+1) * (this->pegs+1); ++i) {
+		if (this->answers[i] > max) {
+			max = this->answers[i];
+			maxI = i;
+		}
+		this->answers[i] = 0;
+	}
+
+	for (allowed_seq_it = this->allowed_seq.begin(); allowed_seq_it != this->allowed_seq.end(); ) {
+		if (this->get_answer(*allowed_seq_it, query) != maxI) {
+			this->allowed_seq.erase(allowed_seq_it++);
+		}
+		else {
+			allowed_seq_it++;
 		}
 	}
 
-	if (reply_i == pegs)
-		correct = 1;
+	int* reply = new int[this->pegs]();
 
-	for (int i = 0; i < pegs; ++i) {
-		if (query[i] != answer[i]) {
-			if (copy_colors[query[i]]) {
-				copy_colors[query[i]]--;
-				reply[reply_i++] = 1;
-			}
-		}
+	i = 0;
+	while (maxI >= (this->pegs+1)) {
+		reply[i] = 2;
+		i++;
+		maxI -= (this->pegs+1);
 	}
 
-	for (;reply_i < pegs; ++reply_i) {
-		reply[reply_i] = 0;
+	while (maxI > 0) {
+		reply[i] = 1;
+		i++;
+		maxI--;
+	}
+
+	this->correct = 1;
+	for (i = 0; i < this->pegs; ++i) {
+		if (reply[i] != 2) {
+			this->correct = 0;
+			break;
+		}
 	}
 
 	return reply;
--- mefisto.h
+++ mefisto.h
@@ -2,19 +2,28 @@
 #define _MEFISTO_H
 
 #include "macros.h"
+#include <list>
 
 class Mefisto {
-	int ai_id;
-	int pegs;
-	int colors;
-	int answer[MAX_PINS];
-	int colors_ans[MAX_COLORS];
-	int correct;
+	private:
+		int ai_id;
+		int pegs;
+		int colors;
+		int answer[MAX_PINS];
+		int colors_ans[MAX_COLORS];
+		int correct;
+		int nseq;
+
+		int* answers;
+		std::list<int> allowed_seq;
+
+		void prepare_allowed_seq();
+		int get_answer(int, int*);
 
 	public:
-	Mefisto(int, int, int);
-	int* get_reply(int*);
-	int is_correct();
+		Mefisto(int, int, int);
+		int* get_reply(int*);
+		int is_correct();
 };
 
 #endif
